3a4d6aa849998ca952c35f896e33b717d65ac171
Himanshi Jain <Himanshi Jain@LAPTOP-0RLLHN1N>
2023-07-08 18:02:36 +0530

    Add project files.

---
+++ TrainingWebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TrainingWebAppMVC.Models;
+
+namespace TrainingWebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Greet()
+        {
+            return View();
+        }
+
+        public IActionResult Sample(int num1=0,int num2=0)
+        {
+            int sum = num1 + num2;
+            ViewBag.addition = sum;
+            return View();
+        }
+        public IActionResult Emp() 
+        {
+            Employee employee = new Employee()
+            {
+                EmpId = 1,
+                FirstName = "Himanshi",
+                LastName = "Jain",
+                Salary = 650000
+            };
+
+            ViewBag.emp = employee;
+
+            return View();
+        }
+
+        public IActionResult EmployeeForm()
+        {
+            return View(new Employee());
+        }
+
+        public IActionResult DisplayEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(employee);
+            }
+            else
+            {
+                return View("EmployeeForm");
+            }
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TrainingWebAppMVC/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingWebAppMVC.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Employee ID is required")]
+        public int? EmpId { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Salary is required")]
+        public double? Salary { get; set; }
+    }
+}
